b2f9a2921f09858de36c943b0739fba70dea6a31
CLiv <livchris89@gmail.com>
2025-03-02 18:06:16 -0800

    1. The beginning

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import *
+
+def main():
+    node = TextNode("This is some anchor text",TextType.LINK, "htts://boot.dev")
+    print(node)
+    
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORAMAL = "Normal text"
+    BOLD = "**Bold text"
+    ITALIC = "_Italic text_"
+    CODE = "`Code`"
+    LINK = "[anchor text](url)"
+    IMAGE = "[alt text](url)"
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+        
+        
+    def __eq__(self, other):
+        if isinstance(other, TextNode):
+            return (
+                self.text == other.text and 
+                self.text_type == other.text_type and 
+                self.url == other.url
+            )
+        return False
+        
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
